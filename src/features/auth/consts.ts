b70b95cc6c47612f42a5ee12645c37f8c6894dc0
Ben Kinsey <bkinsey@gmail.com>
2024-10-21 14:07:29 -1000

    session timeout 4 hours

--- src/features/auth/consts.ts
+++ src/features/auth/consts.ts
@@ -1,4 +1,4 @@
-export const SESSION_TIMEOUT_SECONDS = 2 * 60 * 60; // 2 hours
+export const SESSION_TIMEOUT_SECONDS = 4 * 60 * 60; // 4 hours
 export const SESSION_POLLING_INTERVAL_SECONDS = 5;
 /** amount of warning before session expires */
 export const SESSION_EXPIRE_WARNING_SECONDS = 45;
