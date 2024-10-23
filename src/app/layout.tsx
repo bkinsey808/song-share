e1c2135765c85547d58818a17e24ee98d63c3e55
Ben Kinsey <bkinsey@gmail.com>
2024-10-22 17:23:32 -1000

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className={"lg:overflow-hidden"}>
-			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<link
+				rel="icon"
+				type="image/png"
+				href="/favicon-48x48.png"
+				sizes="48x48"
+			/>
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<meta name="apple-mobile-web-app-title" content="BardoShare" />
 			<body className={`dark ${geistSans.variable} ${geistMono.variable}`}>
 				<AuthProvider>{children}</AuthProvider>
 				<Toaster />
